430323dd4ed5a6be046955067a3a512ce2a74f4c
DrMikeMaik <dr.mike.maik@gmail.com>
2023-09-09 19:00:31 +0200

    Use elevenlabs python library instead of API

--- speech.py
+++ speech.py
@@ -1,34 +1,19 @@
-import requests
 import openai
 from pathlib import Path
-from pydub import AudioSegment
-from pydub.playback import play
+from elevenlabs import generate, play, set_api_key
 from config import API_KEY, EL_API_KEY, CHATBOT
+
+set_api_key(EL_API_KEY)
 
 
 def text_to_speech(text, voice_id):
-    url = f'https://api.elevenlabs.io/v1/text-to-speech/{voice_id}'
-    headers = {
-        'Accept': 'audio/mpeg',
-        'xi-api-key': EL_API_KEY,
-        'Content-Type': 'application/json'
-    }
-    data = {
-        'text': text,
-        'model_id': 'eleven_multilingual_v1',
-        'voice_settings': {
-            'stability': 0.6,
-            'similarity_boost': 0.85
-        }
-    }
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        with open('output.mp3', 'wb') as f:
-            f.write(response.content)
-        audio = AudioSegment.from_mp3('output.mp3')
-        play(audio)
-    else:
-        print('Error:', response.text)
+    audio = generate(
+        text=text,
+        voice=voice_id,
+        model='eleven_multilingual_v1'
+    )
+
+    play(audio)
 
 
 def transcribe(audio, input_language):
